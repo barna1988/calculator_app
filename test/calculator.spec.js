51ddc5cf9629cd5da2ed49741644a1ca5518dfaf
Basavaraj K N <Basavaraj.K@Stackroute.in>
2018-04-05 11:00:05 +0530

    Seed code for solving calculator module based application in node.js

---
+++ test/calculator.spec.js
@@ -0,0 +1,13 @@
+const assert = require('chai').assert;
+const calculator = undefined; // @TODO include your calculator module
+
+// Expected interface of calculator method is as below
+/* function calculate(operation, {lhs, rhs}) */
+
+describe('calculator testing', function() {
+  describe('Addition functionality testing', function() {});
+  describe('Subtraction functionality testing', function() {});
+  describe('Multiplication functionality testing', function() {});
+  describe('Division functionality testing', function() {});
+});
+
