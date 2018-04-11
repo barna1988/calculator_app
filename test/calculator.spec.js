b7a5bfb40239efa6106e368057166a278ea1db5d
Basavaraj K N <Basavaraj.K@Stackroute.in>
2018-04-11 11:05:42 +0530

    Minor corrections due to ESLinting

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -5,60 +5,60 @@ const calculator = undefined; //@TODO include your calculator module
 /* function calculate(operation, {lhs, rhs}) */
 
 describe('calculator testing', function() {
-  describe('Addition functionality testing', function() {
-  	it('Add two positive numbers, returning get positive sum', function() {
-    });
+	describe('Addition functionality testing', function() {
+		it('Add two positive numbers, returning get positive sum', function() {
+		});
 
-    it('Add two negative numbers, returning get negative sum', function() {
-    });
+		it('Add two negative numbers, returning get negative sum', function() {
+		});
 
-    it('Add two number, with either of them is negative, producing subtracted output', function() {
-    });
+		it('Add two number, with either of them is negative, producing subtracted output', function() {
+		});
 
-    it('Add zeros, produces zero', function() {
-    });
-  });
+		it('Add zeros, produces zero', function() {
+		});
+	});
 
-  describe('Subtraction functionality testing', function() {
-    it('Subtract two positive numbers, returning get positive subtraction', function() {
-    });
+	describe('Subtraction functionality testing', function() {
+		it('Subtract two positive numbers, returning get positive subtraction', function() {
+		});
 
-    it('Subtract two negative numbers, returning get negative subtraction', function() {
-    });
+		it('Subtract two negative numbers, returning get negative subtraction', function() {
+		});
 
-    it('Subtract two number, with either of them is negative, producing sum output', function() {
-    });
+		it('Subtract two number, with either of them is negative, producing sum output', function() {
+		});
 
-    it('Subtract zeros, produces zero', function() {
-    });
-  });
-  describe('Multiplication functionality testing', function() {
+		it('Subtract zeros, produces zero', function() {
+		});
+	});
+	describe('Multiplication functionality testing', function() {
 
-    it('Multiply two positive numbers, returning get positive Multiplication', function() {
-    });
+		it('Multiply two positive numbers, returning get positive Multiplication', function() {
+		});
 
-    it('Multiply two negative numbers, returning get positive Multiplication', function() {
-    });
+		it('Multiply two negative numbers, returning get positive Multiplication', function() {
+		});
 
-    it('Multiply two number, with either of them is negative, producing negative multiplication output', function() {
-    });
+		it('Multiply two number, with either of them is negative, producing negative multiplication output', function() {
+		});
 
-    it('Multiply zeros, produces zero', function() {
-    });
-  });
+		it('Multiply zeros, produces zero', function() {
+		});
+	});
 
-  describe('Division functionality testing', function() {
-    it('Divide two positive numbers, returning get positive Multiplication', function() {
-    });
+	describe('Division functionality testing', function() {
+		it('Divide two positive numbers, returning get positive Multiplication', function() {
+		});
 
-    it('Divide two negative numbers, returning get positive Multiplication', function() {
-    });
+		it('Divide two negative numbers, returning get positive Multiplication', function() {
+		});
 
-    it('Divide two number, with either of them is negative, producing negative Division output', function() {
-    });
+		it('Divide two number, with either of them is negative, producing negative Division output', function() {
+		});
 
-    it(`Should not divide by 0, producing 'Can't divide by zero' message`, function() {
-    });
-  });
+		it(`Should not divide by 0, producing 'Can't divide by zero' message`, function() {
+		});
+	});
 });
 
